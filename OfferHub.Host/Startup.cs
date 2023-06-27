8d75218da21d9c0f3a3ea0d9034afb4750b5ee60
User <gold.like.simple@gmail.com>
2023-06-28 00:02:51 +1000

    --init

---
+++ OfferHub.Host/Startup.cs
@@ -0,0 +1,56 @@
+﻿namespace OfferHub.Host;
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public IWebHostEnvironment Environment { get; }
+
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        Configuration = configuration;
+        Environment = environment;
+    }
+
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(
+            options =>
+            {
+                options.AddPolicy(
+                    "AllowOrigin",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200", "https://localhost:4200")
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    }
+                );
+            }
+        );
+
+        SwaggerStartup.ConfigureServices(services);
+        RegisterDatabaseServices(services);
+
+    }
+
+
+    private void RegisterDatabaseServices(IServiceCollection services) { }
+
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        SwaggerStartup.Configure(app, env);
+        
+        app.UseCors("AllowOrigin");
+        app.UseRouting();
+
+        app.UseEndpoints(
+            endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+    }
+}
---
+++ OfferHub.Host/SwaggerStartup.cs
@@ -0,0 +1,37 @@
+﻿namespace OfferHub.Host;
+
+using Microsoft.OpenApi.Models;
+
+static internal class SwaggerStartup
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSwaggerGen(
+            c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Main Api", Version = "v1"});
+                c.SwaggerDoc("admin", new OpenApiInfo {Title = "Admin API", Version = "admin"});
+            }
+        );
+    }
+
+    public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsProduction())
+        {
+            return;
+        }
+
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+
+        app.UseSwaggerUI(
+            c =>
+            {
+                c.RoutePrefix = "docs";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Main API V1");
+                c.SwaggerEndpoint("/swagger/admin/swagger.json", "Admin Api");
+            }
+        );
+    }
+}
