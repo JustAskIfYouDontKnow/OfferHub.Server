57fcba07728ad8f1c4af3e1d99d624deb2fbbb57
User <gold.like.simple@gmail.com>
2023-06-28 19:22:42 +1000

    [+] Supplier Controller [+] Offer Controller

--- OfferHub.Host/Controllers/Client/OfferController.cs
+++ OfferHub.Host/Controllers/Client/OfferController.cs
@@ -1,4 +1,6 @@
-﻿using OfferHub.Host.Services;
+﻿using Microsoft.AspNetCore.Mvc;
+using OfferHub.API.Models.Offer;
+using OfferHub.Host.Services;
 
 namespace OfferHub.Host.Controllers.Client;
 
@@ -6,5 +8,78 @@ public class OfferController : AbstractClientController
 {
     public OfferController(IServiceFactory serviceFactory) : base(serviceFactory) { }
     
+    [HttpPost]
+    [ProducesResponseType(typeof(GetOneOffer.Response), 200)]
+    public async Task<IActionResult> GetOneOfferById([FromBody] GetOneOffer offerRequest)
+    {
+        var offer = await ServiceFactory.OfferService.GetOneById(offerRequest.Id);
+
+        return Ok(new GetOneOffer.Response(
+                offer.Id,
+                offer.Brand,
+                offer.Model,
+                offer.RegistrationDate,
+                offer.SupplierId,
+                offer.Supplier.Name));
+    }
     
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(CreateOffer.Response), 200)]
+    public async Task<IActionResult> Create([FromBody] CreateOffer offerRequest)
+    {
+        var offer = await ServiceFactory.OfferService.Create(offerRequest.Brand, offerRequest.Model, offerRequest.SupplierId);
+
+        return Ok(
+            new CreateOffer.Response(
+                offer.Id,
+                offer.Brand,
+                offer.Model,
+                offer.RegistrationDate,
+                offer.SupplierId));
+    }
+
+
+    [HttpGet]
+    [ProducesResponseType(typeof(OfferList.Response), 200)]
+    public async Task<IActionResult> SearchOffers(string searchTerm)
+    {
+        var fundedOffers = await ServiceFactory.OfferService.SearchOffers(searchTerm);
+        
+        var response = new OfferListWithTotalCount.Response
+        {
+            TotalCount = fundedOffers.Count,
+            Offers = fundedOffers.Select(o => new OfferList.Response(
+                o.Id,
+                o.Brand,
+                o.Model,
+                o.Supplier.Name,
+                o.RegistrationDate
+            )).ToList()
+        };
+    
+        return Ok(response);
+    }
+    
+    
+    [HttpGet]
+    [ProducesResponseType(typeof(OfferListWithTotalCount.Response), 200)]
+    public async Task<IActionResult> ListOffers()
+    {
+        var offerList = await ServiceFactory.OfferService.OfferList();
+
+        var response = new OfferListWithTotalCount.Response
+        {
+            TotalCount = offerList.Count,
+            Offers = offerList.Select(o => new OfferList.Response(
+                o.Id,
+                o.Brand,
+                o.Model,
+                o.Supplier.Name,
+                o.RegistrationDate
+            )).ToList()
+        };
+    
+        return Ok(response);
+    }
 }
--- OfferHub.Host/Controllers/Client/SupplierController.cs
+++ OfferHub.Host/Controllers/Client/SupplierController.cs
@@ -1,4 +1,6 @@
-﻿using OfferHub.Host.Services;
+﻿using Microsoft.AspNetCore.Mvc;
+using OfferHub.API.Models.Supplier;
+using OfferHub.Host.Services;
 
 namespace OfferHub.Host.Controllers.Client;
 
@@ -6,5 +8,50 @@ public class SupplierController : AbstractClientController
 {
     public SupplierController(IServiceFactory serviceFactory) : base(serviceFactory) { }
     
+    [HttpPost]
+    [ProducesResponseType(typeof(CreateSupplier.Response), 200)]
+    public async Task<IActionResult> CreateSupplier([FromBody] CreateSupplier supplierRequest)
+    {
+        var supplier = await ServiceFactory.SupplierService.Create(supplierRequest.Name);
+
+        return Ok(new CreateSupplier.Response(
+                supplier.Id,
+                supplier.Name,
+                supplier.CreationDate));
+    }
     
+    
+    [HttpGet]
+    [ProducesResponseType(typeof(PopularitySupplierList.Response), 200)]
+    public async Task<IActionResult> GetPopularitySupplier()
+    {
+        const int maxTakeSupplier = 3;
+        var supplierList = await ServiceFactory.SupplierService.SupplierList(0, maxTakeSupplier);
+
+        var response = supplierList
+            .Select(s => new PopularitySupplierList.Response(
+                    s.Name,
+                    s.Offers.Count))
+            .OrderByDescending(supplier => supplier.OfferCount)
+            .ToList();
+        
+        return Ok(response);
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(SupplierList.Response), 200)]
+    public async Task<IActionResult> SupplierList(int skip, int take)
+    {
+        var supplierList = await ServiceFactory.SupplierService.SupplierList(skip, take);
+
+        var response = supplierList
+            .Select(s => new SupplierList.Response(
+                    s.Id,
+                    s.Name,
+                    s.CreationDate,
+                    s.Offers.Select(o => o.Id).ToList()))
+            .ToList();
+        
+        return Ok(response);
+    }
 }
